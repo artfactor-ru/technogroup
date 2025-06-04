311de950c84bdc5adf738435cf599c9cc81a8c35
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2025-06-04 15:42:41 +0300

    закончил страницу о компании

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -103,3 +103,237 @@ if (document.querySelector(".metal__slider")) {
 		on: {},
 	});
 }
+
+if (document.querySelector(".mountain__slider")) {
+	// Указываем скласс нужного слайдера
+	// Создаем слайдер
+	new Swiper(".mountain__slider", {
+		// Указываем скласс нужного слайдера
+		// Подключаем модули слайдера
+		// для конкретного случая
+		modules: [Navigation],
+		observer: true,
+		observeParents: true,
+		slidesPerView: 1,
+		spaceBetween: 0,
+		autoHeight: true,
+		speed: 800,
+
+		//touchRatio: 0,
+		//simulateTouch: false,
+		//loop: true,
+		//preloadImages: false,
+		//lazy: true,
+
+		/*
+      // Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+		// Пагинация
+		/*
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			*/
+
+		// Скроллбар
+		/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+		// Кнопки "влево/вправо"
+		navigation: {
+			prevEl: ".mountain-prev",
+			nextEl: ".mountain-next",
+		},
+		/*
+			// Брейкпоинты
+			breakpoints: {
+				640: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+		// События
+		on: {},
+	});
+}
+
+if (document.querySelector(".fertilizers__slider")) {
+	// Указываем скласс нужного слайдера
+	// Создаем слайдер
+	new Swiper(".fertilizers__slider", {
+		// Указываем скласс нужного слайдера
+		// Подключаем модули слайдера
+		// для конкретного случая
+		modules: [Navigation],
+		observer: true,
+		observeParents: true,
+		slidesPerView: 1,
+		spaceBetween: 0,
+		autoHeight: true,
+		speed: 800,
+
+		//touchRatio: 0,
+		//simulateTouch: false,
+		//loop: true,
+		//preloadImages: false,
+		//lazy: true,
+
+		/*
+      // Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+		// Пагинация
+		/*
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			*/
+
+		// Скроллбар
+		/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+		// Кнопки "влево/вправо"
+		navigation: {
+			prevEl: ".fertilizers-prev",
+			nextEl: ".fertilizers-next",
+		},
+		/*
+			// Брейкпоинты
+			breakpoints: {
+				640: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+		// События
+		on: {},
+	});
+}
+
+if (document.querySelector(".about-trust__slider")) {
+	// Указываем скласс нужного слайдера
+	// Создаем слайдер
+	new Swiper(".about-trust__slider", {
+		// Указываем скласс нужного слайдера
+		// Подключаем модули слайдера
+		// для конкретного случая
+		modules: [Navigation],
+		observer: true,
+		observeParents: true,
+		slidesPerView: 5,
+		spaceBetween: 20,
+		autoHeight: true,
+		speed: 800,
+
+		//touchRatio: 0,
+		//simulateTouch: false,
+		//loop: true,
+		//preloadImages: false,
+		//lazy: true,
+
+		/*
+      // Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+		// Пагинация
+		/*
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			*/
+
+		// Скроллбар
+		/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+		// Кнопки "влево/вправо"
+		navigation: {
+			prevEl: ".about-trust-prev",
+			nextEl: ".about-trust-next",
+		},
+		/*
+			// Брейкпоинты
+			breakpoints: {
+				640: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+		// События
+		on: {},
+	});
+}
